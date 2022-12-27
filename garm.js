2c70d5cb5d0fa56a8d2b2e4f9955b5b48287d895
Sagyndyk <sagyndyk@MacBook-Pro-Sagyndyk.local>
2022-12-28 02:13:06 +0600

    finish mb

--- garm.js
+++ garm.js
@@ -1,8 +1,9 @@
-const ans1 = document.querySelector('#answer1');
-const ans2 = document.querySelector('#answer2');
-const ans3 = document.querySelector('#answer3');
+// 1 шаг = объеденить вопросы и ответы
+// 2 шаг = придумать правльную вариацию функции
+// 3 шаг = открыть ответ
+// 4 шаг = закрыть все остальные ответы
 
-function first(){
+/*function first(){
 	ans1.style.display = 'flex'
 	ans2.style.display = 'none'
 	ans3.style.display = 'none'
@@ -18,4 +19,36 @@ function third(){
 	ans1.style.display = 'none'
 	ans2.style.display = 'none'
 	ans3.style.display = 'flex'
-}
+}*/
+
+document.querySelectorAll('.accordion').forEach((element)=> {
+	//берем все элементы с классом .accordion
+	element.addEventListener('click', () => {
+		//создаем событие по клику
+		let content = element.nextElementSibling;
+		//берем следующий элемент 
+		console.log(content)
+
+		if (content.style.maxHeight){
+			//если в стиле ответа присутствует max-height прировнять его к нулю
+			document.querySelectorAll('.content').forEach((element) => element.style.maxHeight = null)
+		} else {
+			//в другом случае прировнять все другие элементы к нулю и сделать так чтобы именно у следующего элемента после вопроса max-height = высоте самого контента
+			document.querySelectorAll('.content').forEach((element) => element.style.maxHeight = null)
+			content.style.maxHeight = content.scrollHeight + 'px'
+		}
+		 
+	})
+})
+
+//forEach вызывает функцию для каждого элемента
+//nextElementSibling берет следующий элемент (все облегчил спасибо разрабам)))
+//overflow:hidden; скрывает все элементы за пределами блока
+//используем max-height и hidden чтобы манипулировать ответами
+
+
+
+
+
+
+
